47688d2c422dbc626e005f1127cde5cf0e548620
Andrew Hoyer <andrew@inkling.com>
2011-08-19 14:47:28 -0700

    Sprinkled a couple comments and reshuffled a things a little

--- is.js
+++ is.js
@@ -4,21 +4,8 @@
 var CLASS_REGEX = /\[.* (.*)\]/i, // Matches: '[object Date]' => 'Date'
     TO_STRING = Object.prototype.toString;
 
-var basicType = function(obj){
-    return CLASS_REGEX.exec(TO_STRING.apply(obj))[1].toLowerCase();
-};
-
-var customType = function(name, obj){
-    var types = CUSTOM_TYPES[name];
-
-    for (var type in types){
-        if (types[type](obj)) return type;
-    }
-
-    return false;
-};
-
-var domCheck = function(nodeType){
+// Returns a function to be used when comparing nodes.
+var domNodeCheck = function(nodeType){
     return function(obj){
         return obj && obj.nodeType === nodeType;
     };
@@ -27,12 +14,28 @@ var domCheck = function(nodeType){
 var CUSTOM_TYPES = {
     USER: {},
     DOM: {
-        'element' : domCheck(1),
-        'textnode': domCheck(3),
-        'comment' : domCheck(8),
-        'document': domCheck(9),
-        'fragment': domCheck(11)
+        'element' : domNodeCheck(1),
+        'textnode': domNodeCheck(3),
+        'comment' : domNodeCheck(8),
+        'document': domNodeCheck(9),
+        'fragment': domNodeCheck(11)
     }
+};
+
+// Returns the base type of an object as determined by Object.prototype.toString.
+var basicType = function(obj){
+    return CLASS_REGEX.exec(TO_STRING.apply(obj))[1].toLowerCase();
+};
+
+// Returns a custom type if found.
+var customType = function(name, obj){
+    var types = CUSTOM_TYPES[name];
+
+    for (var type in types){
+        if (types[type](obj)) return type;
+    }
+
+    return false;
 };
 
 // Actual is definition:
